130b2608871bdf70522f3ad4db1e82c8ea570b67
Allan <allancolardelle@gmail.com>
2025-03-07 13:27:20 +0200

    Initial deploy

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+
+import ProductList from "@/components/shared/header/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.action";
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
+
+  return (<>
+  <ProductList data={latestProducts} title='Newest Arrival'  />
+  </>)
+}
+ 
+export default Homepage;
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import { insertProductSchema } from "@/lib/constants/validators";
+import { z } from "zod";
+
+export type Product = z.infer<typeof insertProductSchema> & {
+  id: string;
+  rating: string;
+  createdAt: Date;
+};
