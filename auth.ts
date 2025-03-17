865df8e91f32f0dcc6c369e061a4b761693c5914
Allan <allancolardelle@gmail.com>
2025-03-17 07:24:52 +0200

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./prisma";
+import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig} from "next-auth";
-}
+
 
 export const config = {
   pages: {
@@ -52,7 +52,7 @@ export const config = {
       },
     }),
   ],
-  callback: {
+  callbacks: {
     async session({ session, user, trigger, token }: any) {
       //set the user ID from the token
       session.user.id = token.sub;
